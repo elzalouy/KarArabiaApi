6d259217faac2415439303601c991b4685f22fba
Ezat Elzalouy <ezatelzalouy711@gmail.com>
2020-09-05 10:44:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors({ origin: FrontApp }));
 configErrors();
 logging();
 db();
